a694318b33e63d7944a4d650b3b1e1c218575f84
Hyunwoo Kim <artprogrammer@naver.com>
2025-11-16 18:52:28 +0900

    テストコード作成

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -8,6 +11,13 @@ void processInput(GLFWwindow* window);
 int main()
 {
 	std::cout << "Hello, world!" << std::endl;
+
+	// glm test
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f); 
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+	std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
 
 	glfwInit();
 
